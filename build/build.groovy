25c3939ca0454e31106aff9a147d784f78e3704a
Jerry Edgington <edge0504@yahoo.com>
2020-11-05 19:42:13 -0500

    updated build.groovy to match polycephaly

--- build/build.groovy
+++ build/build.groovy
@@ -3,11 +3,13 @@ import com.ibm.dbb.build.*
 import com.zos.java.utilities.*
 
 
-	GroovyObject zBuild = (GroovyObject) ZosAppBuild.newInstance()
- 	def build = zBuild.execute()
+	def GroovyObject zBuild = (GroovyObject) ZosAppBuild.newInstance()
+	// parse command line arguments and load build properties
+	def usage = "build.groovy [options] buildfile"
+ 	def build = zBuild.execute(args, usage)
 	
 	 // define local properties
-	 def properties = BuildProperties.getInstance()
+	 //def properties = BuildProperties.getInstance()
 	 
 	 //def subJob = new SubmitJob()
 	 //def subPDS = "${properties.jclPDS}(TESTJOB)"
